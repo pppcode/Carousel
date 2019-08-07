f74895aa8e41e345fcacf413741d2b707e5274a0
ppp <974270397@qq.com>
2019-08-07 18:00:42 +0800

    组件和动画效果解耦,并实现动画效果

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './index.css'
 
 class Carousel {
-  constructor(root) {
-    this.root = root 
+  constructor(root, animation) {
+    this.animation = animation || ((to, from, onFinish) => onFinish())
+    this.root = root
     this.panels = Array.from(root.querySelectorAll('.panels a'))
     this.dotCt = root.querySelector('.dots')
     this.dots = Array.from(root.querySelectorAll('.dots span'))
@@ -26,23 +27,24 @@ class Carousel {
 
   bind() {
     this.dotCt.onclick = e => {
-      if(e.target.tagName !== 'SPAN') return
+      if (e.target.tagName !== 'SPAN') return
 
+      let lastIndex = this.index
       let index = this.dots.indexOf(e.target)
       this.setDot(index)
-      this.showPage(index)
+      this.showPage(index, lastIndex)
     }
 
     this.pre.onclick = e => {
       let index = this.preIndex
       this.setDot(index)
-      this.showPage(index)
+      this.showPage(index, this.nextIndex)
     }
 
     this.next.onclick = e => {
       let index = this.nextIndex
       this.setDot(index)
-      this.showPage(index)
+      this.showPage(index, this.preIndex)
     }
   }
 
@@ -51,10 +53,92 @@ class Carousel {
     this.dots[index].classList.add('active')
   }
 
-  showPage(index) {
-    this.panels.forEach(panel => panel.style.zIndex = 0)
-    this.panels[index].style.zIndex = 10
+  showPage(toIndex, fromIndex) {
+    // 执行动画，执行完成后最终结果
+    // 如果没传递动画，直接执行结果
+    this.animation(this.panels[toIndex], this.panels[fromIndex], () => {
+      this.panels.forEach(panel => panel.style.zIndex = 0)
+      this.panels[toIndex].style.zIndex = 10
+    })
   }
+
+  setAnimation(animation) {
+    this.animation = animation
+  }
+
 }
 
-const carousel = new Carousel(document.querySelector('.carousel'))
+const Animation = (function(){
+  const css = (node, styles) => Object.entries(styles)
+  .forEach(([key, value]) => node.style[key] = value)
+  const reset = node => node.style = ''
+  
+  return {
+    fade(during = 400) {
+      return function(to, from, onFinish) {
+        console.log(during)
+        css(from, {
+          opacity: 1,
+          zIndex: 10
+        })
+        css(to, {
+          opacity: 0,
+          zIndex: 9
+        })
+
+        setTimeout(() => {
+          css(from, {
+            opacity: 0,
+            transition: `all ${during/1000}s`,
+          })
+          css(to, {
+            opacity: 1,
+            transition: `all ${during/1000}s`,
+          })              
+        }, 100)
+
+
+        setTimeout(() => {
+          reset(from)
+          reset(to)
+          onFinish && onFinish()
+        }, during)
+
+      }
+    },
+
+    zoom(scale = 5, during = 1000) {
+      return function(to, from, onFinish) {
+        css(from, {
+          opacity: 1,
+          transform: `scale(1)`,
+          transition: `all ${during/1000}s`,
+          zIndex: 10
+        })
+        css(to, {
+          zIndex: 9
+        })
+
+        setTimeout(() => {
+          css(from, {
+            opacity: 0,
+            transform: `scale(${scale})`
+          })             
+        }, 100)
+
+        setTimeout(() => {
+          reset(from)
+          reset(to)
+          onFinish && onFinish()
+        }, during)
+
+      }
+    }
+  }
+})()
+
+const carousel = new Carousel(document.querySelector('.carousel'), Animation.fade(300))
+
+document.querySelector('select').onchange = function(e) {
+  carousel.setAnimation(Animation[this.value]())
+}
