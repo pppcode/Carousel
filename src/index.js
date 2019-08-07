01e8e173c0b740ec7249b467d241a6a7f7f1e6d8
ppp <974270397@qq.com>
2019-08-07 10:31:56 +0800

    封装 setDots setPanels 函数,完善README.md

--- src/index.js
+++ src/index.js
@@ -4,40 +4,34 @@ const $ = s => document.querySelector(s)
 const $$ = s => document.querySelectorAll(s)
 
 $('.carousel .dots').onclick = function(e) {
-  // if(e.target.tagName !== 'SAPN') return 
+  if(e.target.tagName !== 'SPAN') return 
   let index = Array.from($$('.carousel .dots span')).indexOf(e.target)
-
-  // 切换小圆点
-  $$('.carousel .dots span').forEach(dot => dot.classList.remove('active'))
-  $$('.carousel .dots span')[index].classList.add('active')
-
-  // 切换对应的面板
-  $$('.carousel .panels a').forEach(panel => panel.style.zIndex = 1)
-  $$('.carousel .panels a')[index].style.zIndex = 10
-
+  setDots(index)
+  setPanels(index)
 }
 
 $('.pre').onclick = function(e) {
   let index = Array.from($$('.carousel .dots span')).indexOf($('.carousel .dots .active'))
   // 计算选中的上一个，+ $$('.carousel .dots span').length 避免得到负数
   index = (index - 1 + $$('.carousel .dots span').length) % $$('.carousel .dots span').length
-  console.log(index)
-
-  $$('.carousel .dots span').forEach(dot => dot.classList.remove('active'))
-  $$('.carousel .dots span')[index].classList.add('active')
-
-  $$('.carousel .panels a').forEach(panel => panel.style.zIndex = 1)
-  $$('.carousel .panels a')[index].style.zIndex = 10
+  setDots(index)
+  setPanels(index)
 }
 
 $('.next').onclick = function(e) {
   let index = Array.from($$('.carousel .dots span')).indexOf($('.carousel .dots .active'))
   index = (index + 1) % $$('.carousel .dots span').length
   console.log(index)
+  setDots(index)
+  setPanels(index)
+}
 
+function setDots(index) {
   $$('.carousel .dots span').forEach(dot => dot.classList.remove('active'))
   $$('.carousel .dots span')[index].classList.add('active')
+}
 
+function setPanels(index) {
   $$('.carousel .panels a').forEach(panel => panel.style.zIndex = 1)
   $$('.carousel .panels a')[index].style.zIndex = 10
 }
